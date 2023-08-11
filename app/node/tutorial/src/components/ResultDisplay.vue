f8990b93cd25837eddb4da9e5b83b9e00d6ea5c3
🌵 <67841820+rotohi@users.noreply.github.com>
2023-08-12 06:18:17 +0900

    途中

---
+++ app/node/tutorial/src/components/ResultDisplay.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import axios from "axios";
+const emit = defineEmits([""])
+defineProps(['features'])
+
+
+const survivalProbability = ref<number | undefined>()
+
+const validationResult: boolean = validateRequestValues()
+
+</script>
+
+<template>
+        <button class="btn btn-primary" v-on:click="displayOutput()">結果を出力</button>
+          <template v-if="survivalProbability !== undefined">
+                    <div class="alert alert-error mt-4 ">
+                      あなたの生存確率は {{ Math.round(survivalProbability) }} % です。
+                    </div>
+          </template>
+
+</template>
